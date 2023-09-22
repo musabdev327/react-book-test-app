d05f8f080157a0d88196f64ae62c8da6b3beb9bc
Muhammad Musab <musabdev327@gmail.com>
2023-09-23 00:28:07 +0500

    adds basename

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <Router>
+        <Router basename="/react-book-test-app">
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/book/:id" element={<BookDetailPage />} />

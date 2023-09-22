3a58673ec0030358f99a22d6ee11103692d49c06
Muhammad Musab <musabdev327@gmail.com>
2023-09-22 22:38:47 +0500

    creates app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 import './App.css';
+import HomePage from './components/HomePage/HomePage';
+import BookDetailPage from './components/BookDetailPage/BookDetailPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/book/:id" element={<BookDetailPage />} />
+          </Routes>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
